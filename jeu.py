bb0d742d04c92e38e6f099d56634c2cb069da108
Olivier Lapointe-Gagne <olivier.lapointe-gagne.1@ulaval.ca>
2019-12-29 20:33:29 -0500

    compteur de point

--- jeu.py
+++ jeu.py
@@ -4,9 +4,13 @@ import random as rn
 
 import time
 
+import numpy as np
+
+
 """
 Définissons des tortues
 """
+goal = tu.Turtle(visible=False)
 bob = tu.Turtle()
 bob.up()
 bob.color('green')
@@ -44,29 +48,24 @@ fen = tu.Screen()
 fen.bgcolor('lightblue')
 
 def mright():
-    if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
-        bob.right(20)
+    for _ in range(6):
+        if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
+            bob.right(7)
+            bob.fd(4)
 
 def mleft():
-    if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
-        bob.left(20)
+    for _ in range(6):
+        if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
+            bob.left(7)
+            bob.fd(4)
 
-def droite():
-    if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
-        bob.right(7)
-        bob.fd(5)
-
-def gauche():
-    if abs(bob.pos()[0]) < 293 and abs(bob.pos()[1]) < 243:
-        bob.left(7)
-        bob.fd(5)
 
 speed = 2
-
+point = 0
 
 def acce():
     global speed
-    speed = 4.5
+    speed = 4
 
 def dcce():
     global speed
@@ -76,26 +75,51 @@ fen.listen()
 
 
 fen.onkey(mright,'Right')
-fen.onkeypress(droite, "Right")
-fen.onkeypress(gauche, 'Left')
 fen.onkey(mleft, 'Left')
 fen.onkey(acce, 'Up')
 fen.onkeypress(acce, 'Up')
 fen.onkeyrelease(dcce, 'Up')
 
 def goalspawn():
-    goal = tu.Turtle(visible=False)
+    goal.speed(0)
     goal.up()
     goal.shape('circle')
     goal.color('red')
-    goal.shapesize(1)
+    goal.shapesize(0.5)
     goal.setpos(rn.randint(-200,200),rn.randint(-200,200))
     goal.showturtle()
 
+def score():
+    global point
+    scorer = tu.Turtle(visible=False)
+    scorer.up()
+    scorer.speed(0)
+    scorer.setpos(-20, 270)
+    scorer.color('lightblue')
+    scorer.begin_fill()
+    scorer.fd(100)
+    scorer.setheading(90)
+    scorer.fd(30)
+    scorer.setheading(180)
+    scorer.fd(110)
+    scorer.setheading(270)
+    scorer.fd(30)
+    scorer.setheading(0)
+    scorer.fd(10)
+    scorer.end_fill()
+    scorer.color("black")
+    scorer.setpos(-10, 270)
+    scorer.write(point, font=("Arial", 20, "normal"))
+    point += 1
+
 goalspawn()
+score()
 
 while True:
     bob.fd(speed)
+    if ((bob.pos()[1] - goal.pos()[1]) ** 2 + (bob.pos()[0] - goal.pos()[0]) ** 2) ** 0.5 < 17:
+        goalspawn()
+        score()
     a = bob.pos()
     if abs(a[0]) >= 296:
         bob.left(180 - 2* bob.heading())
